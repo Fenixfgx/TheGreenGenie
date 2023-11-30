d7cb215ca0d72699970da9911d2e26202839c80a
Fenixfgx <72283013+Fenixfgx@users.noreply.github.com>
2023-11-30 13:23:40 -0500

    Add files via upload

--- GC/script.js
+++ GC/script.js
@@ -2,18 +2,62 @@ function imprimirContenido() {
   const cardContainers = document.querySelectorAll('.cardas');
   let contenido = '';
 
-  // Iniciar desde la segunda tarjeta
-  for (let index = 1; index < cardContainers.length; index++) {
-    contenido += cardContainers[index].outerHTML;
+  cardContainers.forEach((container, index) => {
+    // Verificar si el contenedor está visible en la página actual
+    const isVisible = window.getComputedStyle(container).getPropertyValue('display') !== 'none';
 
-    // Agregar un salto de página después de cada tarjeta, excepto la última
-    if (index < cardContainers.length - 1) {
-      contenido += '<div style="page-break-after: always;"></div>';
+    if (isVisible) {
+      contenido += container.outerHTML;
+
+      // Agregar un salto de página después de cada tarjeta visible, excepto la última
+      if (index < cardContainers.length - 1) {
+        contenido += '<div style="page-break-after: always;"></div>';
+      }
     }
-  }
+  });
 
   const ventanaImpresion = window.open('', '_blank');
   ventanaImpresion.document.write('<html><head><title>Imprimir</title></head><body>' + contenido + '</body></html>');
   ventanaImpresion.print();
   ventanaImpresion.close();
+}
+
+function buscarPorFecha() {
+  const fechaInput = document.getElementById("fechaInput").value;
+
+  const tarjetas = document.querySelectorAll(".cardas");
+  tarjetas.forEach(tarjeta => {
+    const fechaTarjeta = tarjeta.querySelector("#fecha").textContent.trim();
+    if (fechaTarjeta === fechaInput) {
+      tarjeta.style.display = "block";
+    } else {
+      tarjeta.style.display = "none";
+    }
+  });
+}
+window.onload = function () {
+  const tarjetas = document.querySelectorAll(".cardas");
+  tarjetas.forEach(tarjeta => {
+    tarjeta.style.display = "none";
+  });
+};
+
+function actualizarFormatoFecha() {
+  const fechaInput = document.getElementById('fechaIn').value;
+  const fechaFormateada = formatearFecha(fechaInput);
+  document.getElementById('fechaInput').value = fechaFormateada;
+}
+
+function formatearFecha(fecha) {
+  const meses = [
+  "enero", "febrero", "marzo", "abril", "mayo", "junio",
+  "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+
+
+  const partesFecha = fecha.split('-');
+  const año = partesFecha[0];
+  const mes = meses[parseInt(partesFecha[1], 10) - 1];
+  const dia = String(partesFecha[2]).padStart(2, '0');
+
+  return `${dia} de ${mes} de ${año}`;
 }
