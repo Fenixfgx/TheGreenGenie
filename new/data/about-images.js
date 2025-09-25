b9308332114fa56b0a3591706334a4842fadfa6a
Fenixfgx <72283013+Fenixfgx@users.noreply.github.com>
2025-09-25 02:07:58 -0500

    Delete new directory

--- new/data/about-images.js
+++
@@ -1,8 +0,0 @@
-// Imágenes para el slider de About Us (editar este array para agregar/quitar imágenes)
-window.ABOUT_IMAGES = [
-  'img/about/portada/1.jpg',
-  'img/about/portada/2.jpg',
-  'img/about/portada/3.jpg',
-  'img/about/portada/4.jpg',
-  'img/about/portada/5.jpg'
-];
