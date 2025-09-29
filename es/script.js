b5ed335b5173ea7d49e674314e40b4717a9f0b0b
Fenixfgx <72283013+Fenixfgx@users.noreply.github.com>
2025-09-29 13:59:55 -0500

    Add files via upload

--- es/script.js
+++ es/script.js
@@ -167,6 +167,217 @@ counters.forEach(counter => {
   observer.observe(counter);
 });
 
+$('.acordeon').on('click','.acordeon__titulo',function() {
+  var t = $(this);
+  var p = t.parent().siblings().find('.acordeon__contenido');
+  var tp = t.next();
+  var temp = 200;
+  p.slideUp(temp);
+  tp.slideToggle(temp);
+});
+
+window.addEventListener("load", event => {
+
+    var swiperWrapper = document.querySelector('.swiper-wrapper');
+
+    /* The Team */
+    var team = [
+        {
+            name: "Carlos Rodríguez",
+            role: "Quality and Production Supervisor",
+            desc: "Far far away, behind the world mountains, far from the countries Vokalia and Consonantia, theres live the blind texts.",
+            photo: "http://www.thegreengenie.com.co/images/Carlos.jpg",
+            website: "https://rafaelalucas.com",
+            email: "mailto:rafaelavlucas@gmail.com",
+            linkedin: "https://www.linkedin.com/in/rafaelalucas/",
+            dribbble: "https://dribbble.com/rafaelalucas",
+        },
+        {
+            name: "Dina Marcela Ortiz",
+            role: "Founder",
+            desc: "Far far away, behind the world mountains, far from the countries Vokalia and Consonantia, theres live the blind texts.",
+            photo: "http://www.thegreengenie.com.co/images/perf.JPG",
+            website: "https://rafaelalucas.com",
+            email: "mailto:rafaelavlucas@gmail.com",
+            linkedin: "https://www.linkedin.com/in/rafaelalucas/",
+            dribbble: "https://dribbble.com/rafaelalucas",
+        },
+        {
+            name: "Humberto Rodríguez",
+            role: "Founder",
+            desc: "Far far away, behind the world mountains, far from the countries Vokalia and Consonantia, theres live the blind texts.",
+            photo: "http://www.thegreengenie.com.co/images/Humberto.jpg",
+            website: "https://rafaelalucas.com",
+            email: "mailto:rafaelavlucas@gmail.com",
+            linkedin: "https://www.linkedin.com/in/rafaelalucas/",
+            dribbble: "https://dribbble.com/rafaelalucas",
+        },
+        {
+            name: "Angie Lizett",
+            role: "Administrative Coordinator",
+            desc: "Far far away, behind the world mountains, far from the countries Vokalia and Consonantia, theres live the blind texts.",
+            photo: "http://www.thegreengenie.com.co/images/Angie.jpg",
+            website: "https://rafaelalucas.com",
+            email: "mailto:rafaelavlucas@gmail.com",
+            linkedin: "https://www.linkedin.com/in/rafaelalucas/",
+            dribbble: "https://dribbble.com/rafaelalucas",
+        },
+                        {
+            name: "Jazmín Guevara",
+            role: "Sales Coordinator",
+            desc: "Far far away, behind the world mountains, far from the countries Vokalia and Consonantia, theres live the blind texts.",
+            photo: "http://www.thegreengenie.com.co/images/Jazmin.jpg",
+            website: "https://rafaelalucas.com",
+            email: "mailto:rafaelavlucas@gmail.com",
+            linkedin: "https://www.linkedin.com/in/rafaelalucas/",
+            dribbble: "https://dribbble.com/rafaelalucas",
+        },
+    ];
+
+    /* Social Icons */
+    var icons = [{
+        iWebsite: "https://rafaelalucas.com/dailyui/6/assets/link.svg",
+        iEmail: "https://rafaelalucas.com/dailyui/6/assets/email.svg",
+        iLinkedin: "https://rafaelalucas.com/dailyui/6/assets/linkedin.svg",
+        iDribbble: "https://rafaelalucas.com/dailyui/6/assets/dribbble.svg",
+    }];
+
+    var iWebsite = icons[0].iWebsite,
+        iEmail = icons[0].iEmail,
+        iLinkedin = icons[0].iLinkedin,
+        iDribbble = icons[0].iDribbble;
+
+
+    /* Function to populate the team members */
+    function addTeam() {
+        for (let i = 0; i < team.length; i++) {
+
+            /* Variables for the team */
+            var name = team[i].name,
+                role = team[i].role,
+                desc = team[i].desc,
+                photo = team[i].photo,
+                website = team[i].website,
+                email = team[i].email,
+                linkedin = team[i].linkedin,
+                dribbble = team[i].dribbble;
+
+            /* Template for the Team Cards */
+            var template = `
+                <div class="swiper-slide">
+                <div class="card">
+                    <span class="bg"></span>
+                    <span class="more"></span>
+                    <figure class="photo"><img src="${photo}"></figure>
+                        <article class="text">
+                            <p class="name">${name}</p>
+                            <p class="role">${role}</p> 
+                            <p class="desc">${desc}</p> 
+                        </article>
+                        
+                        <div class="social">
+                        <span class="pointer"></span>
+                        <div class="icons">
+                            <a class="icon" href="${website}" target="_blank" data-index="0"><img src="${iWebsite}"></a>
+                            <a class="icon" href="${email}" target="_blank" data-index="1"><img src="${iEmail}"></a>
+                            <a class="icon" href="${linkedin}" target="_blank" data-index="2"><img src="${iLinkedin}"></a>
+                            <a class="icon" href="${dribbble}" target="_blank" data-index="3"><img src="${iDribbble}"></a>
+                            </div>
+                            </div>
+                    </div>
+                </div>`;
+
+            swiperWrapper.insertAdjacentHTML('beforeend', template);
+        }
+    };
+
+
+    addTeam();
+
+
+
+    /* Swiper Settings */
+
+    var mySwiper = new Swiper(".swiper-container", {
+        // Optional parameters
+        direction: "horizontal",
+        loop: true,
+        centeredSlides: false,
+        speed: 800,
+        slidesPerView: 3,
+        spaceBetween: 40,
+        threshold: 5,
+
+
+        // If we need pagination
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev"
+        },
+        breakpoints: {
+            1180: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+                centeredSlides: false,
+            },
+            799: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+                centeredSlides: true,
+                loop: true,
+            },
+        }
+    });
+
+    /* Show More */
+
+    var btnShow = document.querySelectorAll('.more');
+
+
+
+    btnShow.forEach(function (el) {
+        el.addEventListener('click', showMore);
+    });
+
+    function showMore(event) {
+        var card = event.target.closest(".swiper-slide");
+
+        if (card.classList.contains('show-more')) {
+            card.classList.remove('show-more');
+        } else {
+            card.classList.add('show-more')
+        }
+
+    }
+
+
+    /* Social Hover */
+    var icon = document.querySelectorAll('.icon');
+
+    icon.forEach(function (el) {
+        el.addEventListener("mouseenter", followCursor);
+
+    });
+
+
+    function followCursor(event) {
+        var pointer = event.currentTarget.closest(".swiper-slide").querySelector('.pointer'),
+            index = event.currentTarget.dataset.index,
+            sizeIcon = (60 * index) + 25;
+
+        pointer.style.transform = `translateX(${sizeIcon}px)`;
+    }
+
+
+    /* end */
+});
+
 document.addEventListener('DOMContentLoaded', function() {
   // Obtén todos los elementos con la clase "et-hero-tab"
   var tabs = document.querySelectorAll('.et-hero-tab');
@@ -186,18 +397,19 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
-const texts = document.querySelectorAll('.TEXt, .TEXt2, .TEXt3');
-let currentIndex = 0;
 
-function showText() {
-    texts[currentIndex].style.opacity = '0';
-    currentIndex = (currentIndex + 1) % texts.length;
-    texts[currentIndex].style.opacity = '1';
-}
+// Obtener todos los botones por su clase
+var skypeButtons = document.querySelectorAll(".skypeButton");
 
-showText(); // Mostrar el primer texto inmediatamente
+// Agregar un evento de clic a cada botón para redirigir a Skype al hacer clic
+skypeButtons.forEach(function(button) {
+  button.addEventListener("click", function() {
+    var destination = button.getAttribute("data-destination");
+    var skypeURL = "skype:" + destination + "?chat";
+    window.location.href = skypeURL;
+  });
+});
 
-setInterval(showText, 7000); // Cambiar el texto con una transición cada 2 segundos
 
 const searchInput = document.getElementById("search-input");
 const searchResults = document.getElementById("search-results");
